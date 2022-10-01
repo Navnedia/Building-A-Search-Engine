cfef49d6fa3b1a70cbbcbf37d423734d86d8cf7a
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-10-01 03:19:47 -0500

    Created a Tokenizer interface & added a simple implementation

--- indexing_process.py
+++ indexing_process.py
@@ -4,6 +4,7 @@ import dataclasses
 import __future__
 from typing import *
 import json
+import re
 
 
 @dataclasses.dataclass
@@ -139,6 +140,25 @@ class Indexer(ABC):
         pass
 
 
+class Tokenizer(ABC):
+    """
+    A utility class interface for processing text into tokens for searching.
+    This class abstracts the tokenization process to allow for different implementations
+    and parameters.
+    """
+
+    @staticmethod
+    @abc.abstractmethod
+    def tokenize(data: str) -> List[str]:
+        """
+        Processes the raw string input by splitting it into tokens.
+
+        :param data: The raw text string to break into separate tokens
+        :return: A list strings containing the original text separated into tokens
+        """
+        pass
+
+
 class DictDocumentCollection(DocumentCollection):
     """
     A DocumentCollection implementation that operates using a dictionary.
@@ -188,8 +208,25 @@ class WikiJsonDocumentSource(DocumentSource):
         return doc_collection
 
 
+class NaiveTokenizer(Tokenizer):
+    """
+    A utility class for processing text into tokens for searching.
+
+    This class implements tokenization by taking a minimal/naive approach.
+    Text made lowercase and is broken up into tokens of individual words
+    and punctuation separated. Punctuation is NOT removed.
+    """
+
+    @staticmethod
+    def tokenize(data: str) -> List[str]:
+        tokens = re.sub(r'(\W)', r' \1 ', data.lower())  # Split off all non-word chars.
+        tokens = re.sub(r'(\w+)\s(\')\s(\w+)', r'\1\2\3', tokens)  # Fix apostrophes in the middle of words by removing spaces from previous step.
+        tokens = re.sub(r'\.\s+\.\s+\.', r' ...', tokens)  # When we see three periods separated by spaces, group them into one '...' ellipsis token.
+        return tokens.split()  # Split processed string into tokens.
+
+
 class NaiveSearchDocumentTransformer(DocumentTransformer):
-    def __init__(self, tokenizer):
+    def __init__(self, tokenizer: Tokenizer):
         """
         A DocumentTransformer implementation that runs the supplied tokenizer.
 
