79ab3f4ffa3b972fb6a4ebcc55012adc5593abdc
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-10-11 11:21:20 -0500

    Fixed broken test cases

--- tests/test_indexing_process.py
+++ tests/test_indexing_process.py
@@ -7,7 +7,7 @@ class TestDefaultIndexingProcess(TestCase):
     def test_index_default_process(self):
         source = FakeDocumentSource(FakeDocumentCollection.from_str_list(['d1', 'd2']))
         self.assertEqual(source.read().get_doc('0'), InputDocument(doc_id='0', text='d1'))
-        indexing_process = DefaultIndexingProcess(FakeDocumentTransformer(), NaiveIndexer())
+        indexing_process = DefaultIndexingProcess(FakeDocumentTransformer(), NaiveIndexer(''))
         index = indexing_process.run(source)
         self.assertEqual(index.docs, [
             TransformedDocument('0', ['d1']),
