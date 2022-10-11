4877e52f3754435b66cbef9ff26cdcd892188fb4
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-10-11 11:10:51 -0500

    Inverted index and stop word implementations - HW3

---
+++ hw3.py
@@ -0,0 +1,133 @@
+import json
+from collections import Counter, defaultdict
+from counters import CounterBasedTextCounter, count_total_words
+from indexing_process import NaiveTokenizer
+from typing import List, Set, Tuple, Dict
+
+
+def get_small_wiki_text() -> List[str]:
+    """
+    Load in text fields from wiki_small.json
+
+    :return: A list of document text strings.
+    """
+    with open('wiki_small.json') as fp:
+        return [doc['init_text'] for doc in json.load(fp) if doc['init_text']]
+
+
+def dump_to_file(data, filepath: str):
+    """
+    Outputs data into a json file.
+
+    :param data: The data to write to the file.
+    :param filepath: The string path and name of the JSON file to write data to.
+    """
+    with open(filepath, 'w') as fp:
+        json.dump(data, fp)
+
+
+def compute_document_counts(docs: List[str]) -> Counter:
+    """
+    Get a count of how many documents each token appears in.
+
+    :param docs: A list of document text strings.
+    :return: A counter for number of documents each token is in.
+    """
+    tokenizer = NaiveTokenizer()
+    document_counts = Counter()
+    for doc in docs:
+        # Tokenize each document into a set & add update the count totals.
+        tokens = set(tokenizer.tokenize(doc))
+        document_counts.update(tokens)
+
+    return document_counts
+
+
+def compute_stopwords(docs: List[str]) -> Set[str]:
+    """
+    Generate stop words to ignore by picking words that occur often, and in most documents. These
+    words can be ignored because they won't help to narrow down the search results much.
+
+    :param docs: A list of document text strings.
+    :return: A set of stop words to ignore.
+    """
+    total_counts = count_total_words(docs)  # Total counts for tokens in every document.
+    document_counts = compute_document_counts(docs)  # Number of documents each token appears in.
+    most_common = {token for token, count in total_counts.most_common(20)}  # 20 most common tokens over all documents.
+    common_across_docs = {token for token, count in document_counts.items() if count >= 9}  # Tokens that appear in at least 9 documents.
+
+    return most_common.intersection(common_across_docs)  # Tokens in both sets are common enough to be considered irrelevant stop words.
+
+
+def get_best_terms(texts: List[str], stopwords: Set[str]) -> List[List[Tuple[str, int]]]:
+    """
+    Calculate the most useful terms for each document. A term is useful if it is a common
+    word in the document, but is not in the list of stop words.
+
+    :param texts: A list of document text strings.
+    :param stopwords: A Set of word strings to ignore.
+    :return: A list of lists with tuples for the best terms and there associated count.
+    """
+    best_terms = []
+    for text in texts:
+        # Get word counts for each text.
+        word_counter = CounterBasedTextCounter().count_words(text)
+        for word in stopwords:  # Remove stopwords.
+            del word_counter[word]
+        best_terms.append(word_counter.most_common(10))  # Add the 10 most common words for this document.
+
+    return best_terms
+
+
+def create_inverted_index(docs: List[str]) -> Dict[str, Set[int]]:
+    """
+    Generate an inverted index for the document tokens. The inverted index will be a
+    dictionary where the keys are tokens, and the value is a set of document indexes
+    matching the token.
+
+    :param docs: A list of document text strings.
+    :return: The inverted index dictionary.
+    """
+    inverted_index = defaultdict(set)
+    tokenizer = NaiveTokenizer()
+    for i, text in enumerate(docs):
+        tokens = tokenizer.tokenize(text)  # Tokenize each document.
+        for token in tokens:
+            # Add each token in the document to the inverted index. The token is the key,
+            # and we add the current document index to the set of document the token appears in.
+            inverted_index[token].add(i)
+
+    return inverted_index
+
+
+def search_2_words(word1: str, word2: str, index: Dict[str, Set[int]]) -> Set[int]:
+    """
+    Get results for document indexes that match BOTH the two query words.
+
+    :param word1: The first query word to match.
+    :param word2: The second query word to match.
+    :param index: An inverted index dictionary containing the token words as the key,
+        and a set of indexes matching to token as a value. See create_inverted_index.
+    :return: A set of document indexes matching the query words.
+    """
+    return index[word1].intersection(index[word2])  # The set of document indexes that match both words.
+
+
+def search_query(query: str, index: Dict[str, Set[int]]) -> Set[int]:
+    """
+    Get results for document indexes that match ALL the tokens in the query string.
+
+    :param query: A string of query words to match.
+    :param index: An inverted index dictionary containing the token words as the key,
+        and a set of indexes matching to token as a value. See create_inverted_index.
+    :return: A set of document indexes matching the tokens in the query string.
+    """
+    query_tokens = NaiveTokenizer().tokenize(query)
+    token_matches = [index[token] for token in query_tokens]  # Get the index set for each token in the query.
+    # Take the intersection of all the query token matches:
+    search_results = token_matches[0]
+    for i in range(1, len(token_matches)):
+        # Intersection of the current token set, with all the previous intersections. Order doesn't matter.
+        search_results = token_matches[i].intersection(search_results)
+
+    return search_results
---
+++ test_hw3.py
@@ -0,0 +1,85 @@
+import hw3
+from collections import Counter
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_compute_document_counts(self):
+        self.assertEqual(
+            Counter({'a': 3, 'd': 2, 'b': 2, 'c': 1, 'e': 1}),
+            hw3.compute_document_counts([
+                'a b c d',
+                'a b d',
+                'a e'
+            ])
+        )
+
+    def test_compute_stopwords(self):
+        # The document collection (corpus) is generated using:
+        # [' '.join(['stopword1', 'stopword2'] * 2 + ['in_all_docs'] + [f'11_of_these{i}', f'11_of_those{i}'] * 11)
+        #  for i in range(10)]
+        # stopword1 occurres in all 10 docs, total of 20 times
+        # stopword2 occurres in 9 docs, total of 18 times
+        # 11_of_these{i} and 11_of_those{i} each occurres 11 time in a single document with index i.
+        # in_all_docs occurres in all documents, but only occurres 10 time in the collection, so
+        #   it will not make it into top 20 most common tokens.
+        self.assertEqual(
+            {'stopword1', 'stopword2'},
+            hw3.compute_stopwords(
+                [
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0 11_of_these0 11_of_those0',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1 11_of_these1 11_of_those1',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2 11_of_these2 11_of_those2',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3 11_of_these3 11_of_those3',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4 11_of_these4 11_of_those4',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5 11_of_these5 11_of_those5',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6 11_of_these6 11_of_those6',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7 11_of_these7 11_of_those7',
+                    'stopword1 stopword2 stopword1 stopword2 in_all_docs 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8 11_of_these8 11_of_those8',
+                    'stopword1 stopword1 in_all_docs 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9 11_of_these9 11_of_those9'
+                ]
+            )
+        )
+
+
+    def test_get_best_terms(self):
+        self.assertEqual(
+            [[('frequent10', 10), ('frequent9', 9), ('frequent8', 8), ('frequent7', 7),
+              ('frequent6', 6), ('frequent5', 5), ('frequent4', 4), ('frequent3', 3),
+              ('frequent2', 2), ('frequent1', 1)],
+             [('frequent2', 2), ('frequent1', 1)]],
+            hw3.get_best_terms(texts=["""
+stopword1 stopword2 frequent10 frequent10 frequent10 frequent10 frequent10 frequent10 frequent10 frequent10 frequent10 frequent10
+stopword1 stopword2 frequent9 frequent9 frequent9 frequent9 frequent9 frequent9 frequent9 frequent9 frequent9
+stopword1 stopword2 frequent8 frequent8 frequent8 frequent8 frequent8 frequent8 frequent8 frequent8
+stopword1 stopword2 frequent7 frequent7 frequent7 frequent7 frequent7 frequent7 frequent7
+stopword1 stopword2 frequent6 frequent6 frequent6 frequent6 frequent6 frequent6
+stopword1 stopword2 frequent5 frequent5 frequent5 frequent5 frequent5
+stopword1 stopword2 frequent4 frequent4 frequent4 frequent4
+stopword1 stopword2 frequent3 frequent3 frequent3
+stopword1 stopword2 frequent2 frequent2
+stopword1 stopword2 frequent1
+stopword1 stopword2 
+""", """
+stopword1 stopword2 frequent2 frequent2
+stopword1 stopword2 frequent1
+stopword1 stopword2 
+"""],
+                               stopwords={'stopword1', 'stopword2'}))
+
+    def test_create_inverted_index(self):
+        self.assertEqual(
+            {'a': {0}, 'b': {0, 1}, 'c': {0, 1, 2}, 'd': {1, 2}, 'e': {2}},
+            hw3.create_inverted_index(['a b c', 'b c d', 'c d e']))
+
+    def test_search_2_words(self):
+        self.assertEqual(
+            {1},
+            hw3.search_2_words(
+                'b', 'd', index={'a': {0}, 'b': {0, 1}, 'c': {0, 1, 2}, 'd': {1, 2}, 'e': {2}}))
+
+    def test_search_query(self):
+        self.assertEqual(
+            {1, 3},
+            hw3.search_query(
+                'b d e', index={'a': {0}, 'b': {0, 1, 3}, 'c': {0, 1, 2}, 'd': {1, 2, 3}, 'e': {1, 2, 3}}))
