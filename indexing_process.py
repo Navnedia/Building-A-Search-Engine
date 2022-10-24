3e2353f8d39ab54461862a61c122f1b5eea08882
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-10-24 17:30:01 -0500

    Added helper methods to create & run IF-IDF inverted indexing process

--- indexing_process.py
+++ indexing_process.py
@@ -1,6 +1,6 @@
-from document_source import DocumentSource, WikiJsonDocumentSource
+from document_source import DocumentSource, WikiJsonDocumentSource, TrecCovidJsonlSource
 from document_transformer import DocumentTransformer, NaiveSearchDocumentTransformer
-from index import Index, Indexer, NaiveIndexer
+from index import Index, Indexer, NaiveIndexer, DictInvertedIndexer
 from tokenizer import NaiveTokenizer
 
 
@@ -38,27 +38,59 @@ class DefaultIndexingProcess:
 
 def create_naive_indexing_process(indexer_file: str) -> DefaultIndexingProcess:
     """
-    Creates an instance of DefaultIndexingProcess by incorporating the
-    naive indexing components (i.e, NaiveTokenizer, NaiveSearchDocumentTransformer, NaiveIndexer/NaiveIndex).
+    Creates an instance of DefaultIndexingProcess by incorporating the naive indexing components
+    (i.e, NaiveTokenizer, NaiveSearchDocumentTransformer, NaiveIndex).
 
-    :param indexer_file: The JSON file name and path for the NaiveIndex to write to, or read from.
+    :param indexer_file: The JSON file name and path to write the index to.
     :return: An instance of the DefaultIndexingProcess.
     """
-    doc_transformer = NaiveSearchDocumentTransformer(NaiveTokenizer())  # Construct a naive document transformer using the naive tokenizer.
-    indexer = NaiveIndexer(indexer_file)  # Create a naive index/indexer using the indexer file for reading and writing indexed data.
-    return DefaultIndexingProcess(doc_transformer, indexer)  # Return an indexing process using the naive components above.
+    # Construct a naive document transformer using the naive tokenizer.
+    # Create a naive index/indexer using the indexer file for reading and writing indexed data.
+    # Return an indexing process using the naive components above.
+    return DefaultIndexingProcess(
+        document_transformer=NaiveSearchDocumentTransformer(NaiveTokenizer()),
+        indexer=NaiveIndexer(indexer_file))
 
 
 def run_naive_indexing_process(input_file: str, output_file: str) -> None:
     """
-    Runs the naive indexing process on the input file and writes the indexed data
-    to the output file.
+    Runs the naive indexing process on the input file and writes the indexed data to the output file.
 
     :param input_file: The JSON input file name and path to read/index.
     :param output_file: The JSON output file name and path to write indexed data to.
     :return: None
     """
-    naive_indexing_process = create_naive_indexing_process(output_file)  # Get an instance of the indexing process using naive components.
-    wiki_source = WikiJsonDocumentSource(input_file)  # Create a document source pointing to the input json file.
-    index = naive_indexing_process.run(wiki_source)  # Run indexing process to build the index for the wiki source.
-    index.write()  # Write the indexed/Transformed data to the output file specified in the indexing process.
+    naive_ip = create_naive_indexing_process(output_file)  # Build an indexing process using naive components.
+    # Run indexing process on wiki json source to build the index.
+    index = naive_ip.run(WikiJsonDocumentSource(input_file))
+    index.write()  # Write the indexed/Transformed data to the output file.
+
+
+def create_inverted_indexing_process(indexer_file: str) -> DefaultIndexingProcess:
+    """
+    Creates an instance of DefaultIndexingProcess using inverted index with TF-IDF.
+    Uses DictBasedInvertedIndexWithFrequencies to build index.
+
+    :param indexer_file: The JSONL file name and path to write the index to.
+    :return: An instance of the DefaultIndexingProcess.
+    """
+    # Construct the indexing process using our dictionary inverted index with TF-IDF.
+    return DefaultIndexingProcess(
+        document_transformer=NaiveSearchDocumentTransformer(NaiveTokenizer()),
+        indexer=DictInvertedIndexer(indexer_file))
+
+
+def run_covid_inverted_indexing_process(input_file: str, output_file: str) -> None:
+    """
+    Runs the TF-IDF inverted indexing process for the Trec covid corpus. Writes index to
+    output file.
+
+    :param input_file: The JSONL input file name and path to read/index.
+    :param output_file: The JSONL output file name and path to write indexed data to.
+    :return: None
+    """
+    # Build an indexing process using TF-IDF inverted index components.
+    inverted_ip = create_inverted_indexing_process(output_file)
+    # Run indexing process on covid jsonl source to build the index.
+    index = inverted_ip.run(TrecCovidJsonlSource(input_file))
+    index.write()  # Write the index data to the output file.
