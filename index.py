ec1a8660b7225f979e9a7797c2a3d4966960f19d
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-11-23 01:02:02 -0600

    Rewrote the Dictionary index search function to support alternatives using OR logic

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import math
 from abc import ABC
 from collections import defaultdict, Counter
 from dataclasses import asdict
-from typing import List
+from typing import List, Dict, Set
 
 from documents import TransformedDocument
 from search_api import Query, SearchResults
@@ -241,7 +241,7 @@ class DictBasedInvertedIndexWithFrequencies(Index):
         Index that is the final output of the Indexing Process and the main source for Query Process.
 
         A Dictionary based implementation to speed up information writing & retrieval. Also includes
-        ordering results by TF-IDF.
+        ordering results by TF-IDF, and support for alternative term OR queries.
 
         :param file_path: The string path and name of the JSONL file to read and write the index to.
         """
@@ -260,30 +260,65 @@ class DictBasedInvertedIndexWithFrequencies(Index):
             # Add this doc_id and term frequency to the current terms inverted index entry.
             self.term_to_doc_id_and_frequencies[term][doc.doc_id] = term_frequency(count, len(doc.tokens))
 
+    def _score_matches_for_term(self, term: str, ids_matching_all_terms: Set[str], doc_match_scores: Dict[str, float]):
+        """
+        A search helper function to calculate and update the TF-IDF score for all documents containing the term.
+
+        :param term: The term to calculate a score for.
+        :param ids_matching_all_terms: A set of document ids that match the whole query for ALL terms (or at
+            least one of the terms alternatives).
+        :param doc_match_scores: A dictionary mapping the doc_id to it's total match score against the whole query.
+        """
+        # If the term isn't in the index, then we can return early to avoid a divide by 0 error.
+        if self.doc_counts[term] <= 0:
+            return
+
+        # Calculate the TF-IDF for each matching document that contains the current term, and add it to the document query score.
+        idf = inverse_document_frequency(self.doc_counts[term], self.num_documents)
+        for doc_id in ids_matching_all_terms:
+            if doc_id in self.term_to_doc_id_and_frequencies[term]:
+                doc_match_scores[doc_id] += self.term_to_doc_id_and_frequencies[term][doc_id] * idf
+
     def search(self, query: Query) -> SearchResults:
         # Get only the doc_ids that match ALL the terms in the query. Intersection of the
         # keys in the dictionary of each term.
-        matches_all_terms = None
+        ids_matching_all_terms = None
         for term in query.terms:
-            # Results must include every term, so if no documents match all the terms, or if
-            # the term isn't in the index, then return an empty result early.
-            if term not in self.term_to_doc_id_and_frequencies or matches_all_terms == set():
-                return SearchResults([])
-            if not matches_all_terms:  # If the set is empty (a.k.a. for the first term).
-                matches_all_terms = self.term_to_doc_id_and_frequencies[term].keys()
-            else:
-                matches_all_terms &= self.term_to_doc_id_and_frequencies[term].keys()
+            # Get doc_ids that match the original term, OR any of its alternatives using union:
+            term_matches = set()
+            if term in self.term_to_doc_id_and_frequencies:  # If the original term is in the index record the doc_ids.
+                term_matches = self.term_to_doc_id_and_frequencies[term].keys()
+            for alternative in query.alternatives[term]:  # Union ids for alternatives if they exist in the index.
+                if alternative in self.term_to_doc_id_and_frequencies:
+                    term_matches |= self.term_to_doc_id_and_frequencies[alternative].keys()
 
+            if ids_matching_all_terms is None:  # If the set is empty (a.k.a. for the first term).
+                ids_matching_all_terms = term_matches
+            else:
+                ids_matching_all_terms &= term_matches  # Intersection with previous terms to get the docs in common.
+
+            # Results must include every term (or one of its alternatives). If the set of matching ids is
+            # empty, then we have no matches that match all the terms, so return an empty result early.
+            if ids_matching_all_terms == set():
+                return SearchResults([])
+
+        # Incase terms share synonyms, maintain a list of alternatives we have already processed,
+        # so we don't process them more than once.
+        processed_alternatives = []
         # Calculate the total query match score for each matching document:
-        match_scores = defaultdict(float)
+        doc_match_scores = defaultdict(float)
         for term in query.terms:
-            idf = inverse_document_frequency(self.doc_counts[term], self.num_documents)
-            # Calculate the TF-IDF for each matching document, and add it to the document query score.
-            for doc_id in matches_all_terms:
-                match_scores[doc_id] += self.term_to_doc_id_and_frequencies[term][doc_id] * idf
-            # Return the correct number of SearchResults ordered by the TF-IDF total query score.
-            sorted_results = sorted(match_scores.keys(), key=match_scores.get, reverse=True)
-            return SearchResults(sorted_results[:query.num_results])
+            self._score_matches_for_term(term, ids_matching_all_terms, doc_match_scores)  # Call scoring helper for original term.
+            # Call the scoring helper for each of the unprocessed term alternatives:
+            for alternative in query.alternatives[term]:
+                if alternative in processed_alternatives:
+                    continue
+                self._score_matches_for_term(alternative, ids_matching_all_terms, doc_match_scores)
+                processed_alternatives.append(alternative)
+
+        # Return the correct number of SearchResults ordered by the TF-IDF total query score (highest to lowest).
+        sorted_results = sorted(doc_match_scores.keys(), key=doc_match_scores.get, reverse=True)
+        return SearchResults(sorted_results[:query.num_results])
 
     def read(self):
         with open(self.file_path, 'r') as fp:
