f3d11fee47cdfc2ffa86bfa40e028ce54cce50cf
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-11-23 00:49:35 -0600

    Implemented thesaurus query expansion and added a synonyms source document
     I created and implementation of TermAlternativesSource specifically for loading in data from JSONL synonym alternatives. I also made an  implementation for the QueryExpander to get synonym alternatives for a given list of query terms. Finally, I have added a sample synonym alternatives source file.

--- query_expansion.py
+++ query_expansion.py
@@ -54,3 +54,45 @@ class TermAlternativesSource(ABC):
         :return: A instance of QueryExpander with all the data loaded in.
         """
         pass
+
+
+class ThesaurusQueryExpander(QueryExpander):
+    def __init__(self, term_alternatives: Dict[str, List[str]]):
+        self.term_alternatives = term_alternatives
+
+    def add_alternatives(self, term: str, alternatives: List[str]) -> None:
+        # Add alternative for a term, unless alternatives list is empty.
+        if isinstance(alternatives, list) and len(alternatives) > 0:
+            self.term_alternatives[term] = alternatives
+
+    def get_alternatives(self, term: str) -> List[str]:
+        return self.term_alternatives.get(term, [])
+
+    def process_query(self, query_terms: List[str]) -> Dict[str, List[str]]:
+        synonyms = defaultdict(list)
+        # Build a dictionary mapping the original query terms to a list of alternatives (synonyms):
+        for term in query_terms:
+            if term in self.term_alternatives:  # Don't include the term if we don't have alternatives for it.
+                synonyms[term] = self.term_alternatives[term]
+
+        return synonyms
+
+
+class JsonlThesaurusTermAlternativesSource(TermAlternativesSource):
+    def __init__(self, file_path: str):
+        """
+        A source reader for loading in thesaurus data in JSONL format.
+
+        :param file_path: The filename and path to read data from.
+        """
+        self.file_path = file_path
+
+    def read(self) -> QueryExpander:
+        alternatives = defaultdict(list)
+        # Load in alternative records from the file path:
+        with open(self.file_path, 'r') as fp:
+            for line in fp:
+                record = json.loads(line)
+                if record['syns']:  # Only record the record if the alternatives (synonyms) list isn't empty.
+                    alternatives[record['term']] = record['syns']
+        return ThesaurusQueryExpander(alternatives)  # Return a Query Expander with all the loaded data.
